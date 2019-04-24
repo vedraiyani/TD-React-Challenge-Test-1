e90cb6e22d619fb4319fdc81c886aae982c5ad67
Chintan Raiyani <chintan.raiyani@sap.com>
2019-04-25 03:05:36 +0530

    Updates

---
+++ precache-manifest.cdb3e101e16a6aeb7085a5aa52e20a90.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/TD-React-Challenge-Test-1/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "a54ef4f6ee099f9afcb5",
+    "url": "/TD-React-Challenge-Test-1/static/css/main.5322b5d7.chunk.css"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/TD-React-Challenge-Test-1/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "5def01841529e81aa27b",
+    "url": "/TD-React-Challenge-Test-1/static/js/1.5def0184.chunk.js"
+  },
+  {
+    "revision": "0d868d1c74a756ad1658",
+    "url": "/TD-React-Challenge-Test-1/static/js/runtime~main.0d868d1c.js"
+  },
+  {
+    "revision": "a54ef4f6ee099f9afcb5",
+    "url": "/TD-React-Challenge-Test-1/static/js/main.a54ef4f6.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/TD-React-Challenge-Test-1/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/TD-React-Challenge-Test-1/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/TD-React-Challenge-Test-1/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "5def01841529e81aa27b",
+    "url": "/TD-React-Challenge-Test-1/static/css/1.c1b20f52.chunk.css"
+  },
+  {
+    "revision": "bc5585e193c614d32b3d7bd0d3483f9c",
+    "url": "/TD-React-Challenge-Test-1/index.html"
+  }
+];
